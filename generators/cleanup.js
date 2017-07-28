d004dfa8523a0e34074e110bc4e8edd2a5d91f11
JulienMrgrd <julien.margarido@gmail.com>
2017-07-28 16:51:34 +0200

    test before renaming bean

--- generators/cleanup.js
+++ generators/cleanup.js
@@ -66,7 +66,6 @@ function cleanupOldServerFiles(generator, javaDir, testDir) {
     if (generator.isJhipsterVersionLessThan('4.0.0')) {
         generator.removeFile(`${javaDir}async/ExceptionHandlingAsyncTaskExecutor.java`);
         generator.removeFile(`${javaDir}async/package-info.java`);
-        generator.removeFile(`${javaDir}config/jHipsterProperties.java`);
         generator.removeFile(`${javaDir}config/LoadBalancedResourceDetails.java`);
         generator.removeFile(`${javaDir}config/ElasticSearchConfiguration.java`);
         generator.removeFile(`${javaDir}config/apidoc/package-info.java`);
